cb5bb310205b5b0fe8df9692153bd90e9a5f7d8b
3mad Hany <144627820+3madhani@users.noreply.github.com>
2025-08-21 12:07:38 +0300

    Refactor _NodeWidget to use AnimatedStartIndicator and change to StatelessWidget; update pubspec.yaml and pubspec.lock to include percent_indicator dependency

--- lib/features/bible_path/presentation/widgets/chapter_node_widget.dart
+++ lib/features/bible_path/presentation/widgets/chapter_node_widget.dart
@@ -3,6 +3,8 @@ import 'dart:math' as math;
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
+import 'animated_start_indicator.dart';
+
 enum NodeStatus { locked, current, completed }
 
 class PathNode {
@@ -37,61 +39,19 @@ class SnakeNodesPath extends StatelessWidget {
   }
 }
 
-class _NodeWidget extends StatefulWidget {
+class _NodeWidget extends StatelessWidget {
   final PathNode node;
 
   const _NodeWidget({required this.node});
-
-  @override
-  State<_NodeWidget> createState() => _NodeWidgetState();
-}
-
-class _NodeWidgetState extends State<_NodeWidget>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<double> _animation;
 
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
-        if (widget.node.status == NodeStatus.current)
-          AnimatedBuilder(
-            animation: _animation,
-            builder: (context, child) {
-              return Transform.translate(
-                offset: Offset(0, _animation.value),
-                child: child,
-              );
-            },
-            child: const Text(
-              'ابدأ',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-          ),
-        SvgPicture.asset(widget.node.svgPath, width: 80, height: 80),
+        if (node.status == NodeStatus.current) AnimatedStartIndicator(),
+        SvgPicture.asset(node.svgPath, width: 80, height: 80),
       ],
     );
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 600),
-    )..repeat(reverse: true);
-
-    _animation = Tween(
-      begin: -5.0,
-      end: 5.0,
-    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeInOut));
   }
 }
